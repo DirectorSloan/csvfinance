f9267f7e7e1a131a95ab9a096c0b1d84a923561e
bencarsten <ben.langenberg@bencarsten.de>
2013-11-27 09:13:59 +0100

    patched the insert function because of the date bug

--- insert.php
+++ insert.php
@@ -45,14 +45,17 @@ function check_Insert($csvfile) {
    while (($data = fgetcsv($csvhandle, 300, ";")) !== FALSE) 
       	 {
 		$num = count($data);
-		# DEBUG       print_r($data);
+#		print_r($data);
         	
 		$datum = $data[2]; if($datum == "Valutadatum") {continue;}
+		# patch nachdem die Sparkasse in ihren CSVs 2013 statt 13 benutzt
+                $teil2 = substr($datum, -2); $teil1 = substr($datum, 0, 6); $datum = $teil1; $datum .= $teil2;
 		$bemerkung = trim($data[4]);
 		$kontakt = trim($data[5]);
 		$betrag = $data[8];
 		# Check ob Datensatz neu sobald einmal aktiviert bleibt es auf aktiv
 		if($nowinsert == 1){ echo "EINTRAG IN DB: "; intodatabase($datum,$betrag,$kontakt,$bemerkung);}
+# echo "Check: ".$datum." vs. Enddatum: ".$end_datum." Kontakt: ".$kontakt." vs. Endkontakt: ".$end_destination." -> Variable nowinsert -> ".$nowinsert."\n";
 		# eigentlicher Check ob aktueller Durchlauf dem letzten eintrag in der Bank entspricht
 		if($datum == $end_datum){ if($kontakt == $end_destination) { $nowinsert = 1;}}
 	 }	
