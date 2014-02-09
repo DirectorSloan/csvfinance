72d82b36fb76bc9c63097e8f7f83ce6c191c4533
bencarsten <git@bencarsten.de>
2014-02-09 22:11:44 +0100

    funktionierende Version mit Sqlite3 Objektorientierte Version seit PHP 5.4 erzwungen
    Wildes verwenden von new->BenSQLite3 Klasse um.
    
    Vermischung von funktionaler und objektorientierter Programmierung :-(

--- insert.php
+++ insert.php
@@ -1,4 +1,5 @@
 <?php
+#error_reporting('warn');
 # Datenbank Verbindung herstellen und Datenbank wechseln
 # SQLITE CONNECT
 include('sqlite_connect.inc.php');
@@ -8,28 +9,36 @@ $first_param = $_SERVER['argv'][1];
 echo $first_param;
 $original_csv = $first_param;
 $inverted_csv = "inverted_".$original_csv;
-$retval = system("tac $original_csv > '$inverted_csv'");
+#$retval = system("tac $original_csv > '$inverted_csv'"); # Muss auskommentiert werden da kurz mit Dummykopf gefüllt für initiierung
 $csvfile = $inverted_csv;
+
 
 # Einfache Funktion welche Variablen in die Umsätze DB schreibt
 function intodatabase($datum,$betrag,$kontakt,$bemerkung){
+
+# Objekt für Datenbank Handle erzeugen
+$db = new BenSQLite();
 # Hier ein Query SQLite
-$query = "INSERT INTO umsaetze SET datum='$datum',betrag='$betrag',destination='$kontakt',bemerkung='$bemerkung'";
+$query = "INSERT INTO umsaetze (datum, betrag, destination, bemerkung) VALUES ('$datum', '$betrag', '$kontakt', '$bemerkung')";
 echo $query."\n";
 $result = $db->query($query);
 }
 
 # Funktion die die Datensätze an der Richtigen stelle einfügt
 function check_Insert($csvfile) {
+
+# Objekt für Datenbank Handle erzeugen
+$db = new BenSQLite();
+
   # Variable $var declarieren
     $var = 0;
   # Query des letzten Eintrags in der DatenBank sortiert nach Datum
-    $query_check = "SELECT umsatz_id,datum,destination FROM umsaetze ORDER BY umsatz_id DESC LIMIT 1";
+    $query_check = "SELECT rowid,datum,destination FROM umsaetze ORDER BY rowid DESC LIMIT 1";
     $results = $db->query($query_check);
    while($arRow = $results->fetchArray())
       {
   # zuweisen der ergebnissatzID auf eine eindimensionale Variablen / Sichern für den Vergleich
-  $end_id = $arRow['umsatz_id'];
+  $end_id = $arRow['rowid'];
   $end_datum = $arRow['datum'];
   $end_destination = $arRow['destination'];
       }
@@ -38,13 +47,20 @@ function check_Insert($csvfile) {
   # Variable welche als insertzeiger dient initialisieren
   $nowinsert = 0;
   # Ausgabe des letzten Vorhandenen datensatzes vor dem neuen Insert
+# EINGRIFF EINMALIGES MANUELLES SETZEN FALLS DB LEER!!!
+# TODO
+# TODO -> csv von Sparkasse muss nun mit Dummywert gefüllt werden
+$end_datum = "01.01.12";
+$end_destination = "Dummy" ;
+#$db->query("INSERT INTO umsaetze (datum, betrag, destination, bemerkung)
+#          VALUES ('01.01.12', '0', 'Dummy', 'Dummy')");
   echo $end_datum."\n";
   echo $end_destination."\n";
   # Einlesen des CSVs mit Übergabe an insertfunktion wenn neue Datensätze erreicht
-   while (($data = fgetcsv($csvhandle, 300, ";")) !== FALSE) 
+   while (($data = fgetcsv($csvhandle, 3000, ";")) !== FALSE) 
       	 {
 		$num = count($data);
-#		print_r($data);
+#print_r($data);
         	
 		$datum = $data[2]; if($datum == "Valutadatum") {continue;}
 		# patch nachdem die Sparkasse in ihren CSVs 2013 statt 13 benutzt
@@ -54,7 +70,7 @@ function check_Insert($csvfile) {
 		$betrag = $data[8];
 		# Check ob Datensatz neu sobald einmal aktiviert bleibt es auf aktiv
 		if($nowinsert == 1){ echo "EINTRAG IN DB: "; intodatabase($datum,$betrag,$kontakt,$bemerkung);}
-# echo "Check: ".$datum." vs. Enddatum: ".$end_datum." Kontakt: ".$kontakt." vs. Endkontakt: ".$end_destination." -> Variable nowinsert -> ".$nowinsert."\n";
+echo "Check: ".$datum." vs. Enddatum: ".$end_datum." Kontakt: ".$kontakt." vs. Endkontakt: ".$end_destination." -> Variable nowinsert -> ".$nowinsert."\n";
 		# eigentlicher Check ob aktueller Durchlauf dem letzten eintrag in der Bank entspricht
 		if($datum == $end_datum){ if($kontakt == $end_destination) { $nowinsert = 1;}}
 	 }	
--- sqlite_connect.inc.php
+++ sqlite_connect.inc.php
@@ -3,17 +3,28 @@
 # written by Ben Langenberg ben.langenberg@bencarsten.de
 # Init File um sqlite Verbindung herzustellen
 
-$filename = "sparkasse.csv";
+$filename = 'sparkasse.csv';
+$createquery = "CREATE TABLE umsaetze ( datum VARCHAR(15), destination VARCHAR(30), bemerkung VARCHAR(50), betrag INT )";
 
 if (!file_exists($filename)) {
 	fopen($filename, "w+");	
 } 
 
-$db = new SQLite3($filename, SQLITE3_OPEN_READWRITE);
+class BenSQLite extends SQLite3
+{
+
+
+  function __construct()
+  {
+    $this->open('sparkasse.csv');
+  }
+
+}
+
 
 if (filesize($filename) == "0") {
-$createquery = "CREATE TABLE umsaetze ( umsatz_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, datum VARCHAR(15), destination VARCHAR(30), bemerkung VARCHAR(50), betrag INT )";
-$result = $db->query($createquery);
+  $db =  new BenSQLite();
+  $db->query($createquery);
 }
 ?>
 
